3664d887f7fb7a488bc390a09d6f4adc1faee6ba
Aadi0706 <97503774+Aadi0706@users.noreply.github.com>
2022-05-01 16:23:56 +0530

    updtaes on portfolio
    title, gitignore, education added

--- src/components/about.js
+++ src/components/about.js
@@ -21,21 +21,26 @@ function About() {
             <h2>Education</h2>
             {/* <br/> */}
             <div>
-              <h4>Intermediate --> 2012-2013</h4>
-             
-              <p>Kendriya Vidyalaya No.2, Sagar | Status-First Division |</p>
+              <h4> ~ Full Stack Web Development --> Oct,2021-Present</h4>
+              <p>Masai School, Bengaluru, Karnataka</p>
+
             </div>
+            <hr/>
             <div>
-              <h4>Bachelor of Engineering --> 2013-2017 </h4>
-              <p>Branch-Electronics & Communication</p>
+              <h4> ~ Master of Arts --> 2019-2021 </h4>
+              <p>Branch - Public Administration</p>
+              <p>Hari Singh Gour, University, Sagar, M.P.  </p>
+              <p>Status - First Division </p>
+            </div>
+            <hr/>
+            <div>
+              <h4> ~ Bachelor of Engineering --> 2013-2017 </h4>
+              <p>Branch - Electronics & Communication</p>
               <p>BTIRT Engineering College, Sironja, M.P.</p>
-              <p>Status-First Division </p>
+              <p>Status - First Division </p>
             </div>
-            <div>
-              <h4>Master of Arts --> 2019-2021 </h4>
-              <p>Branch-Public Administration</p>
-              <p>Hari Singh Gour, University, Sagar | Status-First Division |</p>
-            </div>
+            <hr/>
+           
           </div>
         </div>
       </div>
